eb006af3d138a661b5d8c58a0e1c68e1d5ded070
SoraOrihara <caca66z@gmail.com>
2025-08-15 11:36:46 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
 
-
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
