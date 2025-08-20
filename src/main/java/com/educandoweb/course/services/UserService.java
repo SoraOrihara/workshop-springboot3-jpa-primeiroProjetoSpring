6777c9bff5e65ebf58d2bc596fa910c36be8e577
SoraOrihara <caca66z@gmail.com>
2025-08-20 10:57:30 -0300

    Inserção de User

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -11,17 +11,21 @@ import com.educandoweb.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
+
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+
 }
