afef3cecbea0c32f027cd65bed5bf4ab45b22c18
SoraOrihara <caca66z@gmail.com>
2025-08-21 10:19:40 -0300

    Tratamento de exceção - findById

---
+++ src/main/java/com/educandoweb/course/resources/exeptions/ResourceExeptionHandler.java
@@ -0,0 +1,26 @@
+package com.educandoweb.course.resources.exeptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.educandoweb.course.services.exeptions.ResourceNotFoundExeption;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExeptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundExeption.class)
+	public ResponseEntity<StandardError> resouceNotFound(ResourceNotFoundExeption e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(),status.value() , error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+}
